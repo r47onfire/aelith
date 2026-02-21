1499ce69e7ce0ea8a150426dce44d74773109357
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2026-02-20 21:41:19 -0500

    moved Backolon to its own repository

--- src/scripting/patterns.ts
+++
@@ -1,8 +0,0 @@
-import { SymbolType, Thing, ThingType } from "./thing";
-
-export function convertBlockBodyToPattern(body: Thing[]): Thing {
-    // Don't mutate the input list.
-    body = body.slice();
-    // Step 1: if it starts with an unnamed "...", this means to let ... be a literal everywhere
-
-}
--- src/utils/hash.ts
+++
@@ -1,5 +0,0 @@
-export function javaHash(s: string) {
-    var hash = 0;
-    for (var i = 0; i < s.length; i++) hash = (Math.imul(hash, 31) + s.charCodeAt(i)) | 0;
-    return hash;
-}
